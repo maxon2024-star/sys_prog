935e40961a5a26d9f5872dc98189c10d3d4a8f3d
maxon2024-star <bigztbmstu@gmail.com>
2024-10-05 08:11:03 +0000

    lab4 7c

--- lab4/seven.c
+++ lab4/seven.c
@@ -2,16 +2,15 @@
 
 int main()
 {
-int limit, sum =0;
-scanf("%d",&limit);
-
-for (int n = 0; n <= limit; n++)
+int n;
+scanf("%d",&n);
+int rev=0,rem;
+while(n>0)
 {
-if ((n % 7 && n % 3) !=0  && n % 5 == 0){
-    sum++;
-    //printf("%d\n", n);
+    rem=n%10; //take out the remainder .. so it becomes 5 for 12345
+    rev=rev*10+rem; //multiply the current number by 10 and add this remainder.
+    n=n/10; //divide the number. So it becomes 1234.
 }
-}
-printf("%d", sum);
+printf("%d",rev);
 return 0;
 }
